2b7daf7895fca7ff8a166bb9f148542c0b129f67
Сергей Николаевич <corund1976@gmail.com>
2021-11-29 10:32:55 +0200

    10.32

---
+++ src/routes/index.js
@@ -0,0 +1,3 @@
+export { default as routes } from '/routes';
+export { default as PrivateRoute } from 'routes/PrivateRoute';
+export { default as PublicRoute } from 'routes/PublicRoute';
---
+++ src/routes/routes.js
@@ -0,0 +1,9 @@
+const routes = {
+  home: '/',
+  auth: '/authorization',
+  income: '/income',
+  expense: '/expense',
+  report: '/report',
+};
+
+export default routes;
