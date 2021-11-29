61bb843d4648ad6ede3520a0061d6073c3c0313f
Сергей Николаевич <corund1976@gmail.com>
2021-11-30 00:08:46 +0200

    00.08

--- src/redux/reducers/authReducer.js
+++ src/redux/reducers/authReducer.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import { createReducer } from '@reduxjs/toolkit';
 
-import authActions from 'redux/auth/actions/authActions';
+import authActions from 'redux/actions/authActions';
 
 const tokenInitialState = null;
 
