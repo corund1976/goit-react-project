a1d0331bc823192709c28d1e61e4174612bd7b85
sandra9ermak <sandra1ermak@gmail.com>
2021-11-29 21:08:10 +0200

    3

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,29 @@
 import s from "./Header.module.css";
-import logo from "./logo.png";
+import logo from "../../images/logo.png";
+import defaultUserPhoto from "../../images/defaultUserPhoto.jpg";
+import logoutBtn from "../../images/logoutBtn.png";
 
 const Header = () => {
   return (
     <header className={s.header}>
       <div className={s.navDiv}>
         {/* <p className={s.logo}>Kapu$ta</p> */}
-        <img src={logo} alt="logo" />
+        <img src={logo} alt="logo" className={s.defaultUserPhoto} />
         <div className={s.userDiv}>
-          <p className={s.userName}>Hi, username!</p>
+          <div className={s.userData}>
+            <img
+              src={defaultUserPhoto}
+              alt="defaultUserPhoto"
+              className={s.userPhoto}
+            />
+            <p className={s.userName}>Hi, username!</p>
+          </div>
           <span className={s.span}></span>
           <button type="button" className={s.exitButton}>
-            Exit
+            Выйти
+          </button>
+          <button type="button" className={s.mobileExitBtn}>
+            <img src={logoutBtn} alt="logoutBtn" />
           </button>
         </div>
       </div>
