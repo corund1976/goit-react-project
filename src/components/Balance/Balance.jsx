e3ff78577889e18a20c1c359434cae08b7c8b72c
Alona Melnykova <alonamelnykova@gmail.com>
2021-11-30 11:05:27 +0300

    30Nov11-05

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import s from './Balance.module.css'
 
 const Balance = () => {
+<<<<<<< Updated upstream
     const handleChange = e => {
         const [input, setInput]= useState('00.00')
       const input= setInput(e.target.value)
@@ -13,11 +14,26 @@ const Balance = () => {
             Name
             <input className={s.balance_value}
               name="name"
+=======
+    const [input, setInput]= useState('00.00')
+    const handleChange = e => {
+        const input = setInput(e.target.value)
+    }
+    return (
+        <>
+        <div className={ s.balance_form}>
+            <p className={s.balance_title}>Баланс:</p>
+             {/* <label > 
+            Баланс: */}
+            <input className={s.balance_value}
+              name="balance"
+>>>>>>> Stashed changes
               pattern="^[ 0-9]+$"
               title="поле должно состоять только из цифр"
               required
               type="text"
               placeholder="Введите баланс"
+<<<<<<< Updated upstream
             value ={input}
               onChange={handleChange}
             />
@@ -25,6 +41,16 @@ const Balance = () => {
 
             <button type='submit' className={ s.balance_submit}>Подтвердить</button>
         </div>
+=======
+                    value={input}
+              onChange={handleChange}
+            />
+          {/* </label> */}
+
+            <button type='submit' className={ s.balance_submit}>Подтвердить</button>
+            </div>
+            </>
+>>>>>>> Stashed changes
     )
 }
 export default Balance;
