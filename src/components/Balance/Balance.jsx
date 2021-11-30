09799dc3b544232f44f7e0553d0d55721e48e8b2
Alona Melnykova <alonamelnykova@gmail.com>
2021-11-30 12:27:56 +0300

    30Nov11-05
    Balance component and styles added

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,5 +1,32 @@
-function Balance() {
-  return <h2>Balance</h2>
-};
+import { useState } from "react"
+import s from './Balance.module.css'
 
+const Balance = () => {
+    const [input, setInput]= useState('00.00')
+    const handleChange = e => {
+        const input = setInput(e.target.value)
+    }
+    return (
+        <>
+        <div className={ s.balance_form}>
+            <p className={s.balance_title}>Баланс:</p>
+             {/* <label > 
+            Баланс: */}
+            <input className={s.balance_value}
+              name="balance"
+              pattern="^[ 0-9]+$"
+              title="поле должно состоять только из цифр"
+              required
+              type="text"
+              placeholder="Введите баланс"
+                    value={input}
+              onChange={handleChange}
+            />
+          {/* </label> */}
+
+            <button type='submit' className={ s.balance_submit}>Подтвердить</button>
+            </div>
+            </>
+    )
+}
 export default Balance;
