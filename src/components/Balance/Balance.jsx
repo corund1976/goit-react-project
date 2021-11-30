35575a10df3e709b101d53dbfcf7862de109b3cc
Alona Melnykova <alonamelnykova@gmail.com>
2021-11-30 12:34:31 +0300

    upd Balance markup

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -17,15 +17,13 @@ const Balance = () => {
 =======
     const [input, setInput]= useState('00.00')
     const handleChange = e => {
-        const input = setInput(e.target.value)
+        const inputValue = setInput(e.target.value)
     }
     return (
         <>
         <div className={ s.balance_form}>
             <p className={s.balance_title}>Баланс:</p>
-             {/* <label > 
-            Баланс: */}
-            <input className={s.balance_value}
+              <input className={s.balance_value}
               name="balance"
 >>>>>>> Stashed changes
               pattern="^[ 0-9]+$"
@@ -45,7 +43,6 @@ const Balance = () => {
                     value={input}
               onChange={handleChange}
             />
-          {/* </label> */}
 
             <button type='submit' className={ s.balance_submit}>Подтвердить</button>
             </div>
