19345bd002c7859d185a65592fb498ae5ea01f88
DValeriy <valeriydm20@gmail.com>
2021-12-01 02:23:10 +0200

    done base markup for reports page

---
+++ src/components/ReportSwitch/categoryList.js
@@ -0,0 +1,55 @@
+import sprite from "../../images/category_svg/sprite.svg";
+export const category = [
+  {
+    title: "Продукты",
+    svg: `${sprite}#products`,
+  },
+  {
+    title: "Алкоголь",
+    svg: `${sprite}#alcohol`,
+  },
+  {
+    title: "Развлечения",
+    svg: `${sprite}#entertaiment`,
+  },
+  {
+    title: "Здоровье",
+    svg: `${sprite}#health`,
+  },
+  {
+    title: "Транспорт",
+    svg: `${sprite}#vehicle`,
+  },
+  {
+    title: "Всё для дома",
+    svg: `${sprite}#home`,
+  },
+  {
+    title: "Техника",
+    svg: `${sprite}#technic`,
+  },
+  {
+    title: "Коммуналка и связь",
+    svg: `${sprite}#communication`,
+  },
+  {
+    title: "Спорт и хобби",
+    svg: `${sprite}#sport`,
+  },
+  {
+    title: "Образование",
+    svg: `${sprite}#education`,
+  },
+  {
+    title: "Прочее",
+    svg: `${sprite}#other`,
+  },
+  {
+    title: "З/П",
+    svg: `${sprite}#salary`,
+  },
+  {
+    title: "Доп. доход",
+    svg: `${sprite}#additional-incomes`,
+  },
+];
--- src/services/kapusta-api.js
+++ src/services/kapusta-api.js
@@ -1,33 +1,35 @@
-import axios from 'axios';
+import axios from "axios";
 
-axios.defaults.baseURL = 'https://kapusta-backend.goit.global';
+axios.defaults.baseURL = "https://kapusta-backend.goit.global";
 /* TOKEN */
 const token = {
   set(token) {
     axios.defaults.headers.common.Authorization = `Bearer ${token}`;
   },
   unset() {
-    axios.defaults.headers.common.Authorization = '';
+    axios.defaults.headers.common.Authorization = "";
   },
 };
 /* Auth */
-const register = credentials => axios.post('/auth/register', credentials); //credentials={email,password}
-const login = credentials => axios.post('/auth/login', credentials); //credentials={email,password}
-const logout = () => axios.post('/auth/logout');
-const refresh = sessionId => axios.post('auth/refresh', sessionId); //sessionId={sid}
-const setGoogleToken = () => axios.get('/auth/google');
+const register = (credentials) => axios.post("/auth/register", credentials); //credentials={email,password}
+const login = (credentials) => axios.post("/auth/login", credentials); //credentials={email,password}
+const logout = () => axios.post("/auth/logout");
+const refresh = (sessionId) => axios.post("auth/refresh", sessionId); //sessionId={sid}
+const setGoogleToken = () => axios.get("/auth/google");
 /* Transaction */
-const setIncome = incomeItem => axios.post('/transaction/income', incomeItem); //incomeItem={description,amount,date}
-const getIncome = () => axios.get('/transaction/income');
-const setExpense = expenseItem => axios.post('/transaction/expense', expenseItem); //expenseItem={description,amount,date,category}
-const getExpense = () => axios.get('/transaction/expense');
-const deleteTransaction = transactionId => axios.delete(`/transaction/${transactionId}`);
-const getIncomeCategories = () => axios.get('/transaction/income-categories');
-const getExpenseCategories = () => axios.get('/transaction/expense-categories');
-const getPeriodData = date => axios.get('/transaction/period-data', date); //date=YYYY-MM
+const setIncome = (incomeItem) => axios.post("/transaction/income", incomeItem); //incomeItem={description,amount,date}
+const getIncome = () => axios.get("/transaction/income");
+const setExpense = (expenseItem) =>
+  axios.post("/transaction/expense", expenseItem); //expenseItem={description,amount,date,category}
+const getExpense = () => axios.get("/transaction/expense");
+const deleteTransaction = (transactionId) =>
+  axios.delete(`/transaction/${transactionId}`);
+const getIncomeCategories = () => axios.get("/transaction/income-categories");
+const getExpenseCategories = () => axios.get("/transaction/expense-categories");
+const getPeriodData = (date) => axios.get("/transaction/period-data", date); //date=YYYY-MM
 /* User */
-const updateUserBalance = balance => axios.patch('/user/balance', balance); //balance={newBalance}
-const getUserInfo = () => axios.get('/user');
+const updateUserBalance = (balance) => axios.patch("/user/balance", balance); //balance={newBalance}
+const getUserInfo = () => axios.get("/user");
 // eslint-disable-next-line
 export default {
   token,
@@ -48,5 +50,5 @@ export default {
   getPeriodData,
   // User
   updateUserBalance,
-  getUserInfo
+  getUserInfo,
 };
