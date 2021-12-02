f15a4be6b1a662507caae45e6fee487558833d24
Khmeliuk <86467378+Khmeliuk@users.noreply.github.com>
2021-12-02 03:30:21 +0200

    bacFix

--- src/components/IncomeExpenseTotal/IncomeExpenseTotal.jsx
+++ src/components/IncomeExpenseTotal/IncomeExpenseTotal.jsx
@@ -1,16 +1,23 @@
 import s from "./IncomeExpenseTotal.module.css";
+import { useSelector } from "react-redux";
+
+import {
+  getIncomeIncomeMonthStats,
+  getIncomeExpenseMonthStats,
+} from "redux/transactions/transactionSelectors";
 
 function IncomeExpenseTotal() {
-  const fish = 1000;
-  
+  const expense = useSelector(getIncomeExpenseMonthStats);
+  const income = useSelector(getIncomeIncomeMonthStats);
+
   return (
     <div className={s.container}>
       <p className={s.title}>
-        Расходы:&nbsp; <span className={s.outlayQuantityХ}>- {fish}</span>
+        Расходы:&nbsp; <span className={s.outlayQuantityХ}>- {expense}</span>
       </p>
       <span className={s.vertical}></span>
       <p className={s.title}>
-        Доходы: <span className={s.income}>&nbsp;+ {fish}</span>
+        Доходы: <span className={s.income}>&nbsp;+ {income}</span>
       </p>
     </div>
   );
