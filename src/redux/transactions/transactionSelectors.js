bcf32af2ea8348c2196e2e1d3dfe04a444dec1c1
Сергей Николаевич <corund1976@gmail.com>
2021-12-02 13:22:49 +0200

    13.22

--- src/redux/transactions/transactionSelectors.js
+++ src/redux/transactions/transactionSelectors.js
@@ -2,7 +2,7 @@
 export const getIncomeTransactions = state => state.transactions.incomes;
 export const getExpenseTransactions = state => state.transactions.expenses;
 // Статистика помесячно доходов / расходов
-export const getIncomeIncomeMonthStats = state => state.transactions.incomeMonthStats;
-export const getIncomeExpenseMonthStats = state => state.transactions.expenseMonthStats
+export const getIncomeMonthStats = state => state.transactions.incomeMonthStats;
+export const getExpenseMonthStats = state => state.transactions.expenseMonthStats
 // Массив всех транзакций
 export const getAllTransactions = state => state.transactions.transactions;
