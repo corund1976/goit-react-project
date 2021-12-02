18ba75b4f974989e1f3e241a50efa24a8605089b
sandra9ermak <sandra1ermak@gmail.com>
2021-12-02 13:50:33 +0200

    3

---
+++ src/components/Balance/BalanceModal.jsx
@@ -0,0 +1,18 @@
+import s from "./BalanceModal.module.css";
+
+const BalanceModal = () => {
+  return (
+    <div className={s.fixedModal}>
+      <div className={s.modalDiv}>
+        <p className={s.largeText}>
+          Привет! Для начала работы внеси текущий баланс своего счета!
+        </p>
+        <p className={s.littleText}>
+          Ты не можешь тратить деньги пока их у тебя нет :
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default BalanceModal;
